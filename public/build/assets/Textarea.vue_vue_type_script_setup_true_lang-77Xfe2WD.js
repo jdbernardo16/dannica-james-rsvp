9cf4194ae97a4df564e881e7541a566c04f19f60
Jaydee Bernardo <jdbernardo@Jaydees-Mac-mini.local>
2025-11-05 23:00:36 +0800

    update

---
+++ public/build/assets/Textarea.vue_vue_type_script_setup_true_lang-77Xfe2WD.js
@@ -0,0 +1,21 @@
+import{c as e}from"./createLucideIcon-B-PKUAuj.js";import{c as t}from"./utils-TfETDaq2.js";import{d as n,c as l,x as c,u as i,o as d}from"./app-B5AG7_0H.js";/**
+ * @license lucide-vue-next v0.468.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const h=e("ArrowRightIcon",[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"m12 5 7 7-7 7",key:"xquz4c"}]]);/**
+ * @license lucide-vue-next v0.468.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const y=e("CircleXIcon",[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["path",{d:"m15 9-6 6",key:"1uzhvr"}],["path",{d:"m9 9 6 6",key:"z0biqf"}]]);/**
+ * @license lucide-vue-next v0.468.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const g=e("MinusIcon",[["path",{d:"M5 12h14",key:"1ays0h"}]]);/**
+ * @license lucide-vue-next v0.468.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const b=e("SendIcon",[["path",{d:"M14.536 21.686a.5.5 0 0 0 .937-.024l6.5-19a.496.496 0 0 0-.635-.635l-19 6.5a.5.5 0 0 0-.024.937l7.93 3.18a2 2 0 0 1 1.112 1.11z",key:"1ffxy3"}],["path",{d:"m21.854 2.147-10.94 10.939",key:"12cjpa"}]]),u=["value"],k=n({__name:"Textarea",props:{modelValue:{},class:{}},emits:["update:modelValue"],setup(s){return(a,o)=>(d(),l("textarea",c({class:i(t)("flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",a.$props.class)},a.$attrs,{value:s.modelValue,onInput:o[0]||(o[0]=r=>a.$emit("update:modelValue",r.target.value))}),null,16,u))}});export{h as A,y as C,g as M,b as S,k as _};
